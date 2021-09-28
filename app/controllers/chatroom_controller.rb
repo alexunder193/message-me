2fffa764075b350e4bc68df5f54b8147ed6c696f
Alex Kyriakou <akyriakou@skroutz.gr>
2021-09-28 18:56:37 +0300

    create home and login routes, views and controllers

---
+++ app/controllers/chatroom_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Root controller
+class ChatroomController < ApplicationController
+  def index; end
+end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Login controller
+class SessionsController < ApplicationController
+  def new; end
+
+  def create; end
+
+  def destroy; end
+end
