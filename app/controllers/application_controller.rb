342de5391ac6cd32603a4273d23a97791e1ac0c0
Alex Kyriakou <akyriakou@skroutz.gr>
2021-10-03 13:50:35 +0300

    Login page html and helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
+# frozen_string_literal: true
+
+# Helper methods for authentication
 class ApplicationController < ActionController::Base
+
+  # We put helper methods in those two def because we use them to views
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user
+    unless logged_in?
+      flash[:error] = 'You must be logged in to perform that action'
+      redirect_to login_path
+    end
+    return if logged_in?
+  end
+
 end
