0c9e48abcf88e8579e1676f3a64ff66ac1f339e8
Alex Kyriakou <akyriakou@skroutz.gr>
2021-10-04 21:37:02 +0300

    Refactor session create controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,7 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(username: params[:session][:username])
-    if user&.authenticate(params[:session][:password])
+    if user_authenticate
       # Authenticate once in next line and remain sign in for future requests
       session[:user_id] = user.id
       flash[:success] = 'You have successfully logged in'
@@ -36,6 +35,11 @@ class SessionsController < ApplicationController
       redirect_to root_path
     end
     return unless logged_in?
+  end
+
+  def user_authenticate
+    user = User.find_by(username: params[:session][:username])
+    user&.authenticate(params[:session][:password])
   end
 
 end
