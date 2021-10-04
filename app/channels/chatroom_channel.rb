1673858d4a5c9a0c41a601fa036554d9898c8d13
Alex Kyriakou <akyriakou@skroutz.gr>
2021-10-04 22:12:39 +0300

    Generate a Chatroom channel

---
+++ app/channels/chatroom_channel.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# Chatroom channel
+class ChatroomChannel < ApplicationCable::Channel
+
+  def subscribed
+    stream_from 'chatroom_channel'
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+end
